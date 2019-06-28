00a22b97905d350d340a7a0bff0382476c26164d
Karanraj Chauhan <chauhank@bu.edu>
2019-06-28 14:02:23 -0400

    Added source code for training model with given params

---
+++ training/generate_hyperparam_combinations.py
@@ -0,0 +1,24 @@
+import itertools
+import yaml
+
+
+if __name__ == "__main__":
+    # file to which hyperparameter sets will be written to
+    OUT_FNAME = "hyperparameters.yml"
+
+    # hyperparameters to tune
+    hyperparams = {
+        'ones': [6, 1, 9],
+        'tens': [4, 7],
+        'hundreds': [3, 5],
+    }
+
+    # generate a list of dicts with specific values for each parameter
+    keys, values = zip(*hyperparams.items())
+    hyperparam_allsets = [dict(hyperparam_set=dict(zip(keys, v))) for v in itertools.product(*values)]
+    print("Total number of hyperparameter sets: "+str(len(hyperparam_allsets)))
+
+    # write to yaml file, each "entry" is a particular realization of hyperparameters
+    with open(OUT_FNAME, 'w') as outfile:
+        yaml.dump(hyperparam_allsets, outfile)
+    print("Hyperparameter sets saved to: " + OUT_FNAME)
