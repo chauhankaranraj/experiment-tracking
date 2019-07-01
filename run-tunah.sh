2fb2e6eee2e6a2b2ffbc9cdedea7c200df5a4e96
Karanraj Chauhan <chauhank@bu.edu>
2019-07-01 10:09:40 -0400

    Removed training project dir and number of jobs hardcoding from run script

--- run-tunah.sh
+++ run-tunah.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-# TODO: remove this hardcoding
-NUM_JOBS=8
+# path to training/hyperparam-tuning project.
+# if not passed as cmd line arg. use default
+# WARNING: You will have to change default according to your project
+TRAIN_DIR=${1-"/home/kachauha/Documents/training"}
+
+# determine number of jobs (one per hyperparam combination)
+NUM_JOBS=$(grep -cow "hyperparam_set" "$TRAIN_DIR/hyperparameters.yml")
 
 # spawn jobs for each set
 # NOTE: since this will be used as in index into an array, it needs to be 0..N-1
